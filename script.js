7c078bec943d09f8bb923cf4043570d4313d75f1
CHANDRAMOULIJOKADA <109852342+CHANDRAMOULIJOKADA@users.noreply.github.com>
2023-06-13 14:29:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function palindrome(str){
 	let n = str.length;
 	let start = 0, end = n-1;
 	while(start<end){
-		char c = str.charAt(start);
-		char c1 = str.charAt(end);
+		let c = str.charAt(start);
+		let c1 = str.charAt(end);
 		if(c != c1) return false;
 		start++;
 		end--;
